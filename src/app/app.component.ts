28878825da6babb1cf5da29456267115c79f08fa
unknown <u2104048@student.cuet.ac.bd>
2025-05-13 09:06:31 +0600

    Login is successful

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet,RouterModule } from '@angular/router';
+import { RouterOutlet,RouterModule, Router, NavigationEnd } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet,RouterModule, HeaderComponent],
+  imports: [RouterOutlet,RouterModule, HeaderComponent,CommonModule],
   templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
   title = 'digital-edu-platform';
+  showHeader = true;
+
+  constructor(private router: Router) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showHeader = !event.url.includes('/login');  // hide header on login
+      }
+    });
+  }
 }
